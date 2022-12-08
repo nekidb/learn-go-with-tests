04721266bcc74cecb87333c7dbd7ceb04b051dec
nekidb <nekidb@gmail.com>
2022-12-08 23:32:39 +0300

    add test for happy scenario and implement it

---
+++ context/context.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
+
+
